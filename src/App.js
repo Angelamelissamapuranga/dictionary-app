e58567597b0a13c2be674acb853bd7c930991a80
Angelamelissamapuranga <angelamapuranga68@gmail.com>
2026-01-20 18:27:59 +0100

    installed axios and added API dictionart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Angela Mapuranga</footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = "0b13ta29a8716f0b00944ca581adofd4";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeyword}
+          placeholder="Search for a word"
+        />
+      </form>
+    </div>
+  );
+}
